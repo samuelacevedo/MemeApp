0234b16d745fd5c1390f83e50ddb845aa18d8539
Samuel Acevedo <acevedo@ubiipagos.com>
2023-06-01 23:22:00 -0400

    Init Project

---
+++ MemeApp/Permissions/View/CameraPermissionsVC.swift
@@ -0,0 +1,16 @@
+//
+//  CameraPermissionsVC.swift
+//  MemeApp
+//
+//  Created by UbiiPagos on 1/6/23.
+//
+
+import UIKit
+
+class CameraPermissionsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
